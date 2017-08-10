7cbd043f480dd0551758bc4952b479fb85d45651
ilgizkasymov <ilgizkasymov@gmail.com>
2017-08-10 10:28:54 +0600

    add request library and crete sensArticle, generateToken getDate methods

---
+++ parser.js
@@ -0,0 +1,82 @@
+let nambaone = 'https://api.namba1.co';
+let request = require('request');
+async function getDateTime() {
+
+    let date = new Date();
+
+    let hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    let min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    let sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    let year = date.getFullYear();
+
+    let month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    let day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    let dash = '-';
+    return year + dash + month + dash + day;
+
+};
+
+async function generateToken(){
+    let data = {
+        url: nambaone + '/users/auth',
+        method: 'POST',
+        body: {
+            'phone': process.env.user,
+            'password': process.env.password
+        },
+        json: true
+    };
+    return new Promise((resolve, reject)=>{
+        request(data, (error, req, body)=>{
+            if(error || req.statusCode === 404){
+                reject(error || new Error('page not found'))
+            }
+            resolve(body.data.token)
+        })
+    })
+}
+
+
+async function sendArticle(groupId, title, body, imgList){
+    let token = await generateToken();
+    let dataForSend = {
+        url:  nambaone + '/groups/' + groupId +'/post',
+        method: 'POST',
+        body: {
+            content: title + '\r\n\r\n' + body,
+            comment_enabled: 1
+        },
+        headers: {
+            'X-Namba-Auth-Token': token,
+        },
+        json: true
+    };
+    if(imgList.length > 0){
+        dataForSend.body['attachments'] = [];
+        for(let i in imgList){
+            dataForSend.body.attachments.push({type: 'media/image', content: imgList[i]})
+        };
+    }
+    return new Promise((resolve, reject)=>{
+        request(dataForSend, (error, req, body)=>{
+            if(error || req.statusCode === 404){
+                reject(error || new Error('not page found'))
+            }
+            resolve(req.statusCode)
+        })
+    })
+}
+
+
+module.exports = sendArticle;
+module.exports = generateToken;
+module.exports = getDateTime;
