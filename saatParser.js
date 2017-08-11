6722d00c5307b671fa76fe451627e9d2939a9d01
ilgizkasymov <ilgizkasymov@gmail.com>
2017-08-11 16:59:37 +0600

    add saat parser

---
+++ saatParser.js
@@ -0,0 +1,91 @@
+let parser = require('./parser');
+let Xray = require('x-ray');
+let x = Xray();
+let xpath = require('xpath')
+    , dom = require('xmldom').DOMParser;
+let request = require('request');
+let ch = require('cheerio');
+let client = require('redis').createClient('redis://h:p8d8e6b778f4b5086a4d4a328f437f873e72bd40522bc0f75c1132a65c213dc3e@ec2-34-231-155-48.compute-1.amazonaws.com:30949');
+
+async function getArticleHtml(url){
+    return new Promise((resolve, reject)=>{
+        let data = {
+            url:url,
+            method: 'GET'
+        };
+        request(data, (error, req, body)=>{
+            if(error || req.statusCode === 404){
+                reject(error || new Error('page not found'))
+            }
+            resolve(body)
+        })
+    })
+}
+async function getArticleBody(url){
+    let html = await getArticleHtml(url);
+    return new Promise((resolve, reject)=>{
+        x(html, 'article', ['.td-post-content p'])((error, textList)=>{
+            resolve(textList.join('\n'))
+        })
+    });
+}
+async function getArticleTheme(url){
+    return new Promise((resolve, reject)=>{
+        x(url, 'title')((error, title)=>{
+            if(!error){
+                resolve(title)
+            }
+            reject(error)
+        })
+    });
+}
+
+async function getArticleImages(url){
+    let html = await getArticleHtml(url);
+    return new Promise((resolve, reject)=>{
+        x(html, 'article', '.td-post-content .td-post-featured-image img@src')((error, imgList)=>{
+            if(!error){
+                resolve(imgList)
+            }
+            reject(error)
+        })
+    });
+}
+let url = 'http://saat.kg/';
+
+async function getUrlList(){
+    return new Promise((resolve, reject)=>{
+        x(url, '.wpb_wrapper .vc_tta-container .vc_general.vc_tta.vc_tta-tabs', ['.td-block-row h3 a@href'])((error, urlList)=>{
+            if(!error){
+                resolve(urlList)
+            }
+            reject(error)
+        })
+    })
+}
+
+async function send(url){
+    let body = await getArticleBody(url);
+    let title = await getArticleTheme(url);
+    let img = await getArticleImages(url);
+    let token = await parser.getImageToken(img)
+    let result = await parser.send(1188, title, body, [token]);
+    console.log(result)
+}
+let dataName = 'saat_test';
+async function start(){
+    let list = await getUrlList();
+    let urlList = list.reverse();
+    client.get(dataName, (error, value)=>{
+        let cutList = urlList.slice(urlList.indexOf(value) + 1);
+        if(cutList.length > 0){
+            cutList.forEach((elem)=>{
+                send(elem)
+            });
+            client.set(dataName, cutList.slice(-1)[0])
+        }else{
+            console.log('Not List')
+        }
+    });
+}
+start();
--- test.js
+++ test.js
@@ -4,5 +4,5 @@
 let client = require('redis').createClient('redis://h:p8d8e6b778f4b5086a4d4a328f437f873e72bd40522bc0f75c1132a65c213dc3e@ec2-34-231-155-48.compute-1.amazonaws.com:30949');
 
 // client.set('azattyk_test_ru', 'https://rus.azattyk.org/a/28670707.html')
-client.set('kloop_test_ru', 'https://kloop.kg/blog/2017/08/11/storonniki-tekebaeva-prekratili-golodovku-iz-za-nedopuska-politika-k-sdache-testa-po-gosyazyku/')
-client.set('kloop_test_kg', 'http://ky.kloop.asia/2017/08/11/kyrgyzstan-fifanyn-rejtinginde-bir-sapka-yldyjlap-128-orundu-eeledi/')
+// client.set('kloop_test_ru', 'https://kloop.kg/blog/2017/08/11/storonniki-tekebaeva-prekratili-golodovku-iz-za-nedopuska-politika-k-sdache-testa-po-gosyazyku/')
+client.set('saat_test', 'http://saat.kg/2017/ala-buka-rajonunun-azhek-ajylyndagy-55-turak-zhaj-sel-korkunuchunan-korgoldu/');
