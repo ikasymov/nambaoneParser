ff8cdcf8c1383cb9578d4b9141364b19f60a089f
ilgizkasymov <ilgizkasymov@gmail.com>
2017-08-14 14:58:30 +0600

    redefined function get body

--- techcrunchParser.js
+++ techcrunchParser.js
@@ -19,6 +19,18 @@ Parser.prototype.getArticleImages = async function () {
     return token
 };
 
+Parser.prototype.getArticleBody = async function(){
+    let html = await this.getArticleHtml();
+    return new Promise((resolve, reject)=>{
+        x(html, this.bodyPath1, this.bodyPath2)((error, textList)=>{
+            if(!error){
+                resolve(textList.join('\n').slice(0, 155) + '.... To continue, press \n' + this.site)
+            }
+            reject(error)
+        })
+    })
+};
+
 let url = 'https://techcrunch.com/';
 async function getUrlList(){
     return new Promise((resolve, reject)=>{
