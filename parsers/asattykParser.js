3d0c40f8e471fba134c8cd4246f8721fc8b9a9ad
ilgizkasymov <ilgizkasymov@gmail.com>
2017-10-06 14:46:30 +0600

    change group

--- parsers/asattykParser.js
+++ parsers/asattykParser.js
@@ -3,27 +3,27 @@ let x = Xray();
 let start = require('../parser').start;
 let Handler = require('../handlerStep');
 let send = require('../send');
-let ruGroup = 1679;
-let kgGroup = 1679;
+let ruGroup = 1181;
+let kgGroup = 1180;
 
-let dataRU = {
-    site: 'http://www.elle.ru/celebrities/novosty/smi-ravshana-kurkova-vyishla-zamuj/',
-    bodyPath1: '#content',
-    bodyPath2: ['.wsw p'],
-    imgPath1: '#content .row',
-    imgPath2: ['.cover-media .img-wrap img@src'],
-    group: 1181,
-    dataName: 'azattyk_test_ru'
-};
-let dataKG = {
-    site: 'http://www.elle.ru/celebrities/novosty/smi-ravshana-kurkova-vyishla-zamuj/',
-    bodyPath1: '#content',
-    bodyPath2: ['.wsw p'],
-    imgPath1: '#content .row',
-    imgPath2: ['.cover-media .img-wrap img@src'],
-    group: 1180,
-    dataName: 'azattyk_test_kg'
-};
+// let dataRU = {
+//     site: 'http://www.elle.ru/celebrities/novosty/smi-ravshana-kurkova-vyishla-zamuj/',
+//     bodyPath1: '#content',
+//     bodyPath2: ['.wsw p'],
+//     imgPath1: '#content .row',
+//     imgPath2: ['.cover-media .img-wrap img@src'],
+//     group: 1181,
+//     dataName: 'azattyk_test_ru'
+// };
+// let dataKG = {
+//     site: 'http://www.elle.ru/celebrities/novosty/smi-ravshana-kurkova-vyishla-zamuj/',
+//     bodyPath1: '#content',
+//     bodyPath2: ['.wsw p'],
+//     imgPath1: '#content .row',
+//     imgPath2: ['.cover-media .img-wrap img@src'],
+//     group: 1180,
+//     dataName: 'azattyk_test_kg'
+// };
 
 async function getUrlList(url){
     return new Promise((resolve, reject)=>{
@@ -43,8 +43,8 @@ async function startParser(){
         let kgUrl = 'https://www.azattyk.org/z/828';
         let ruUrls = await getUrlList(rusUrl);
         let kgUrls = await getUrlList(kgUrl);
-        dataRU.urlList = ruUrls;
-        dataKG.urlList = kgUrls;
+        // dataRU.urlList = ruUrls;
+        // dataKG.urlList = kgUrls;
         let ru = new Handler(ruUrls, 'azattyk_ru');
         let kg = new Handler(kgUrls, 'azattyk_kg');
         let rurl = await ru.getUrl();
